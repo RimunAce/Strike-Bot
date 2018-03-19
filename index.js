37c584b0e4364edcacc1676794aae931d5ded894
Whirl#9077 <whirlplays990@gmail.com>
2018-03-19 18:18:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '>';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', (message) => {
-  if (message.content === 'youtube') {
+  if (message.content.startsWith(prefix + 'youtube')) {
     message.channel.send('Hi, YouTube');
   }
 });
 
-client.login('YOUR TOKEN HERE');
+client.login('TOKEN HERE');
+
