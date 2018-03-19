8a2eb2c99ee71e600ad7f2dee02e6920ba6b40e2
Whirl#9077 <whirlplays990@gmail.com>
2018-03-19 20:14:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity('with Whirl');
+  client.user.setActivity(`On ${client.guilds.size});
 });
 
 client.on('message', (message) => {
