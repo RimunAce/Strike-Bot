bbdee5c066faddee1e51287b2c768a8baf56ddac
Whirl#9077 <whirlplays990@gmail.com>
2018-04-14 15:05:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
 
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
@@ -19,7 +19,7 @@ client.on('message', async (message) => {
     pingm.edit(`Pong! Latency: ${pingm.createdTimestamp - message.createdTimestamp}ms. API Latency: ${Math.round(client.ping)}ms`);
   }
 
-  if(command === "say") {
+  if (command === "say") {
     const sayMessage = args.join(' ');
     message.delete().catch(O_o=>{});
     message.channel.send(sayMessage);
